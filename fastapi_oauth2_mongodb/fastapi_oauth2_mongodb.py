3cde768b0ee53c5f98fb6dcf7f1a136e282edc9c
bohachu <cbh@cameo.tw>
2023-02-26 16:53:33 +0800

    . 清乾淨一些沒用到的 code

--- fastapi_oauth2_mongodb/fastapi_oauth2_mongodb.py
+++ fastapi_oauth2_mongodb/fastapi_oauth2_mongodb.py
@@ -4,50 +4,18 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from starlette.responses import RedirectResponse
-from fastapi_oauth2_mongodb.models import User
+from fastapi_oauth2_mongodb.models import User, TokenData
 from fastapi_oauth2_mongodb.routers_users import router
 import os
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
 app.include_router(router)
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
 
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 
-@app.get("/read_users_me", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
-
-@app.get("/read_own_items")
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
 
 
 @app.get("/")
--- fastapi_oauth2_mongodb/routers_users.py
+++ fastapi_oauth2_mongodb/routers_users.py
@@ -1,13 +1,15 @@
 import pymongo
 import pytest
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, HTTPException
+from passlib.context import CryptContext
+
 from fastapi_oauth2_mongodb.database import collection
-from fastapi_oauth2_mongodb.models import RegisterData, RegisterResult
+from fastapi_oauth2_mongodb.models import RegisterData, RegisterResult, TokenData, User
 from fastapi_oauth2_mongodb.hash import pwd_context
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from typing import Union
-from jose import jwt
+from jose import jwt, JWTError
 import secrets
 
 router = APIRouter()
@@ -63,51 +65,24 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
                 "time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"), "success": False,
                 "message": f"Login failed, exception: {e}"}
 
-@pytest.fixture(scope="module")
-def register_data():
-    return {
-        "username": "testuser",
-        "password": "testpassword",
-        "email": "testuser@example.com",
-    }
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@pytest.fixture(scope="module")
-def login_data():
-    return {
-        "username": "testuser",
-        "password": "testpassword",
-        "grant_type": "password",
-    }
-
-
-def test_login_success(client, register_data, login_data):
-    # register user
-    client.post("/api/users/v1/register", json=register_data)
-    # login
-    response = client.post("/api/users/v1/login", data=login_data)
-    assert response.status_code == 200
-    assert response.json()["success"] is True
-    assert "token" in response.json()
-
-
-def test_login_invalid_username(client, register_data, login_data):
-    # register user
-    client.post("/api/users/v1/register", json=register_data)
-    # try to login with invalid username
-    login_data["username"] = "invalid_username"
-    response = client.post("/api/users/v1/login", data=login_data)
-    assert response.status_code == 200
-    assert response.json()["success"] is False
-    assert response.json()["message"] == "Login failed."
-
-
-def test_login_invalid_password(client, register_data, login_data):
-    # register user
-    client.post("/api/users/v1/register", json=register_data)
-    # try to login with invalid password
-    login_data["password"] = "invalid_password"
-    response = client.post("/api/users/v1/login", data=login_data)
-    assert response.status_code == 200
-    assert response.json()["success"] is False
-    assert response.json()["message"] == "Login failed."
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = collection.find_one({"username": username})
+    return user
