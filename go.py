643b6ae5372db758e3c1fe343d5a3616d8be913f
chiubowen <cbh@cameo.tw>
2023-03-02 15:47:19 +0800

    os.environ['PYTHONPATH'] = '.'

--- go.py
+++ go.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # go.py
+import os
 from os import system as s
 
 import yaml
+
+os.environ['PYTHONPATH'] = '.'
 
 s(f'''python3 -m pip install -r requirements.txt''')
 yml = yaml.safe_load(open("package.yml"))
